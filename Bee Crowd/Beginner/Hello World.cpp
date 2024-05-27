04076d2fae5a53060b17b064561489f7506b670b
JOY045 <shoeb.mahmud0099@gmail.com>
2024-05-27 15:14:06 +0600

    BeeCrowd 1000

---
+++ Bee Crowd/Beginner/Hello World.cpp
@@ -0,0 +1,30 @@
+// Site Name : Bee Crowd
+
+// Problem ID: 1000
+
+// Problem Name:
+// Welcome to beecrowd! Your first program in any programming language is usually "Hello World!". In this first problem all you have to do is print this message on the screen.
+
+// Subject : Sequential
+
+// Timelimit: 1
+
+// Input:
+// This problem has no input.
+
+// Output:
+// You must print the message Hello World! and then the endline as shown below.
+
+// Example (Input & Output):
+// Input: N/A
+
+// Output: Hello World!
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
